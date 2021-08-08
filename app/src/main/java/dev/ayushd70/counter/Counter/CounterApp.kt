f8c4380f8c59a25734b2d90e9f80a7c83a56a7ef
Ayush Dubey <ayushdubey70@gmail.com>
2021-08-08 14:24:47 +0530

    counter: center align the text
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- app/src/main/java/dev/ayushd70/counter/Counter/CounterApp.kt
+++ app/src/main/java/dev/ayushd70/counter/Counter/CounterApp.kt
@@ -1,11 +1,16 @@
 package dev.ayushd70.counter.Counter
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun CounterApp() {
@@ -25,7 +30,11 @@ fun CounterApp() {
         },
         
         content = {
-            Column {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize(),
+            ) {
                 Text(text = "You have pressed the button this many times")
                 Text(text = "4")
             }
