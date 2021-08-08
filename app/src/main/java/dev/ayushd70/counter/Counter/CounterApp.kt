3c0824a13f4f4bd0a3b0091c422f4eb6c014cda8
Ayush Dubey <ayushdubey70@gmail.com>
2021-08-08 14:14:00 +0530

    counter: add incrementing floating action button
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- app/src/main/java/dev/ayushd70/counter/Counter/CounterApp.kt
+++ app/src/main/java/dev/ayushd70/counter/Counter/CounterApp.kt
@@ -1,8 +1,9 @@
 package dev.ayushd70.counter.Counter
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import android.graphics.drawable.Icon
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -14,7 +15,13 @@ fun CounterApp() {
                     Text(text = "Compose Counter App")
                 }
             )
-        }
+        },
+        
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription ="Add")
+            }
+        },
     ) {
     }
 }
